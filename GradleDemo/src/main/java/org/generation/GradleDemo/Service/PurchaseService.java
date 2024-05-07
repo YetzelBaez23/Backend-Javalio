cb283d3fbb950b74a9d2ffc13dd59aa7bb08d8c0
Ulimax <uliortegao@gmail.com>
2024-05-07 16:23:52 -0600

    delete: old release

--- GradleDemo/src/main/java/org/generation/GradleDemo/Service/PurchaseService.java
+++
@@ -1,8 +0,0 @@
-package org.generation.GradleDemo.Service;
-
-import org.generation.GradleDemo.entity.Purchase;
-
-public interface PurchaseService {
-	Purchase getPurchaseById(Long id);
-	Purchase createPurchase(Purchase purchase);
-}
